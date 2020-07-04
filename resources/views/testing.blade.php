12820c704720e0e76c363a33c3c4a477b665e897
kevinmugiira <kevinm5740@aol.com>
2020-07-04 12:10:28 +0300

    added some php

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -4,3 +4,50 @@ echo $username;
 echo "<br />";
 $current_user = $username;
 echo $current_user;
+echo "<br />";
+ $x = 0;
+ if(++$x == 10);
+ echo $x;
+
+echo "<br />";
+
+ $y = 2;
+ if ($y-- == 0);
+ echo $y;
+
+ echo "<br />";
+
+ $w = 2;
+ if (--$w == 0);
+ echo $w;
+
+ echo "<br />";
+
+ $heading = "Date\tName\tPayment";
+ echo $heading;
+
+ echo "<br />";
+
+ $headi =  "Date\nName\nPayment";
+ echo $headi;
+echo "<br />";
+
+$author = "Alfred E Newman";
+echo "This is a headline\r\n
+
+This is the first line.\r\n
+This is the second.\r\n
+Written by $author";
+
+echo"<br />";
+
+$authoor = "the KKK Motherfucker !! ";
+$text = "This is the headline\n
+
+This is the first line.
+This is the second.
+Written by $authoor";
+
+echo $text;
+
+?>
